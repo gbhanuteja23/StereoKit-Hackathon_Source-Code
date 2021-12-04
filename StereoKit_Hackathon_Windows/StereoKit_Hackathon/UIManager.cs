ac2df7ebdb358d512ecee60701c26ffe8261d86b
Shreyansh Anchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2021-12-04 21:31:08 +0530

    Update UIManager.cs

--- StereoKit_Hackathon_Windows/StereoKit_Hackathon/UIManager.cs
+++ StereoKit_Hackathon_Windows/StereoKit_Hackathon/UIManager.cs
@@ -4,43 +4,64 @@ namespace StereoKit_Hackathon
 {
 	class UIManager
 	{
+		string pinchMode = "";
+		string gripMode = "";
+		
+		static Vec3 leftPosition = new Vec3(-0.3f, 0f, -0.3f);
+		static Vec3 rightPosition = new Vec3(0.3f, 0f, -0.3f);
+
+		Pose windowLeftPose = new Pose(leftPosition, Quat.LookAt(leftPosition, Vec3.Zero));
+		Pose windowRightPose = new Pose(rightPosition, Quat.LookAt(rightPosition, Vec3.Zero));
 
 		public void ShowWindow(MeshManager _meshManager)
 		{
-			Pose windowLeftPose = new Pose(-0.3f, 0, -0.3f, Quat.LookAt(Vec3.Forward, Vec3.Zero));
-			Pose windowRightPose = new Pose(0.3f, 0, -0.3f, Quat.LookAt(Vec3.Forward, Vec3.Zero));
+			//left pinch window
+			UI.WindowBegin("Pinch", ref windowLeftPose, moveType: UIMove.FaceUser);
 
-
-			UI.WindowBegin("Pinch", ref windowLeftPose);
+			UI.HSeparator();
+			UI.Text(pinchMode, TextAlign.TopCenter);
+			UI.HSeparator();
 
 			if (UI.Button("Spawn"))
 			{
 				_meshManager.OnPinch = _meshManager.AddCube;
+				pinchMode = "Spawn";
 			}
 			if (UI.Button("Delete"))
 			{
 				_meshManager.OnPinch = _meshManager.RemoveCube;
+				pinchMode = "Delete";
 			}
 			if (UI.Button("Change"))
 			{
 				_meshManager.OnPinch = _meshManager.ChangeCubeColor;
+				pinchMode = "Change";
 			}
 
 			UI.WindowEnd();
 
-			UI.WindowBegin("Grip", ref windowRightPose);
+
+			//right grip window
+			UI.WindowBegin("Grip", ref windowRightPose, moveType: UIMove.FaceUser);
+
+			UI.HSeparator();
+			UI.Text(gripMode, TextAlign.TopCenter);
+			UI.HSeparator();
 
 			if (UI.Button("Spawn"))
 			{
 				_meshManager.OnGrip = _meshManager.AddCube;
+				gripMode = "Spawn";
 			}
 			if (UI.Button("Delete"))
 			{
 				_meshManager.OnGrip = _meshManager.RemoveCube;
+				gripMode = "Delete";
 			}
 			if (UI.Button("Change"))
 			{
 				_meshManager.OnGrip = _meshManager.ChangeCubeColor;
+				gripMode = "Change";
 			}
 
 			UI.WindowEnd();
